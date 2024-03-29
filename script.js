a08d61727ea52398504af05cd3764e1a43ab14a8
renzrph <renzestorrph@gmail.com>
2024-03-29 20:13:42 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,124 @@
+const getDataBtn = document.querySelector(".get-data-button");
+const clearDataBtn = document.querySelector(".clear-data-button");
+const inputElement = document.getElementById("input");
+const activeCases = document.querySelector(".active-cases");
+const newCases = document.querySelector(".new-cases");
+const recoveredCases = document.querySelector(".recovered-cases");
+const totalCases = document.querySelector(".total-cases");
+const totalDeaths = document.querySelector(".total-deaths");
+const totalTests = document.querySelector(".total-tests");
+const countryName = document.querySelector(".country-name");
+const population = document.querySelector(".population");
+const errorMessage = document.querySelector(".error-message");
+const cases = document.querySelectorAll(".cases");
+const loaders = document.querySelectorAll(".loader");
+
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "17b1f86227msh93f8dc5215801b7p1896a2jsn1a24efa1a709",
+    "X-RapidAPI-Host": "covid-193.p.rapidapi.com",
+  },
+};
+
+/* ---------- Helper Functions ---------- */
+const getData = async (url) => {
+  const response = await fetch(url, options);
+  const result = await response.json();
+  console.log(result);
+  return result;
+};
+
+const clearData = () => {
+  countryName.innerHTML = "";
+  population.innerHTML = "";
+  activeCases.innerHTML = "";
+  newCases.innerHTML = "";
+  recoveredCases.innerHTML = "";
+  totalCases.innerHTML = "";
+  totalDeaths.innerHTML = "";
+  totalTests.innerHTML = "";
+  errorMessage.style.display = "none";
+};
+
+const showLoaders = () => {
+  clearData();
+  loaders.forEach((loader) => {
+    loader.style.display = "block";
+  });
+  setTimeout(() => {
+    loaders.forEach((loader) => {
+      loader.style.display = "none";
+    });
+    showStats();
+  }, 2000);
+};
+
+const handleSuccessfulFetch = (result) => {
+  if (result && result.response) {
+    let data = result.response[0];
+    countryName.innerHTML = `Country: ${data.country}`;
+    population.innerHTML = `Populaton: ${data.population.toLocaleString()}`;
+
+    /* Checks if the data is a Number or Null. */
+    typeof data.cases.active === "number"
+      ? (activeCases.innerHTML = data.cases.active.toLocaleString())
+      : (activeCases.innerHTML = "Data not available");
+    typeof data.cases.new === "number"
+      ? (newCases.innerHTML = data.cases.new.toLocaleString())
+      : (newCases.innerHTML = "Data not available");
+    typeof data.cases.recovered === "number"
+      ? (recoveredCases.innerHTML = data.cases.recovered.toLocaleString())
+      : (recoveredCases.innerHTML = "Data not available");
+    typeof data.cases.total === "number"
+      ? (totalCases.innerHTML = data.cases.total.toLocaleString())
+      : (totalCases.innerHTML = "Data not available");
+    typeof data.deaths.total === "number"
+      ? (totalDeaths.innerHTML = data.deaths.total.toLocaleString())
+      : (totalDeaths.innerHTML = "Data not available");
+    typeof data.tests.total === "number"
+      ? (totalTests.innerHTML = data.tests.total.toLocaleString())
+      : (totalTests.innerHTML = "Data not available");
+
+    errorMessage.style.display = "none";
+    inputElement.value = "";
+  }
+};
+
+const handleFetchError = () => {
+  countryName.innerHTML = "Invalid Country Name";
+  population.innerHTML = `Data not available`;
+  activeCases.innerHTML = "Data not available";
+  newCases.innerHTML = "Data not available";
+  errorMessage.style.display = "block";
+  recoveredCases.innerHTML = "Data not available";
+  totalCases.innerHTML = "Data not available";
+  totalDeaths.innerHTML = "Data not available";
+  totalTests.innerHTML = "Data not available";
+};
+
+const showStats = () => {
+  const country = inputElement.value;
+  let url = `https://covid-193.p.rapidapi.com/statistics?country=${country}`;
+
+  getData(url)
+    .then((result) => {
+      handleSuccessfulFetch(result);
+      console.log(result);
+    })
+    .catch((error) => {
+      handleFetchError();
+      console.error(error);
+    });
+};
+
+// Event Listeners
+getDataBtn.addEventListener("click", showLoaders);
+
+clearDataBtn.addEventListener("click", clearData);
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    showLoaders();
+  }
+});
